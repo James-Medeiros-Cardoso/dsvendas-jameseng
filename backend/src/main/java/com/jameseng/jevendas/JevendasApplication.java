1221c3c7bb16ddadf2cab74252d43ad259f6609b
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2021-12-18 14:38:31 -0300

    Criado projeto com backend e frontend

---
+++ backend/src/main/java/com/jameseng/jevendas/JevendasApplication.java
@@ -0,0 +1,13 @@
+package com.jameseng.jevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jameseng/jevendas/JevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameseng.jevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
