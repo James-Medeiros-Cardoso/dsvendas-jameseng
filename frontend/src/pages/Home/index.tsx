001bd4d681ecfb07dd3be65b989e4369c32f8bc1
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2021-12-20 00:20:58 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Página Home.</h1>
+        </div>
+    );
+}
+
+export default Home; //está exportanto a função acima (o componente)
